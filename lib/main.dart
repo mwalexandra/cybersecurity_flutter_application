a5d51110b0e12f0e60a4e1bb09fe65fe588a2b51
Alexandra <manik167@gmail.com>
2025-05-15 14:37:39 +0200

    welcome screen init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 void main() {
   runApp(CybersecurityApp());
 }
@@ -11,12 +13,13 @@ class CybersecurityApp extends StatelessWidget {
     return MaterialApp(
       title: 'Cybersecurity Training App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Color(0xFF1D2A3A),
-        primaryColor: Colors.pinkAccent,
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(color: Colors.white),
-        ),
+      theme: ThemeData(
+        fontFamily: 'Montserrat',
+        scaffoldBackgroundColor: const Color(0xFF1D2A3A),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFFF4A5AE),
+          brightness: Brightness.dark,
+        )
       ),
       home: WelcomeScreen(),
     );
@@ -28,42 +31,69 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal:32.0),
-          child: Column(
-            children: [
-              Icon(Icons.security, size: 100, color: Colors.pinkAccent),
-              const SizedBox(height: 20),
-              Text(
-                "Cybersecurity\nTRAINING APP",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.5,
-                ),
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.pinkAccent,
-                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/images/logo.svg',
+                        width: 200,
+                        height: 200,
+                      ),
+                      const SizedBox(height: 60),
+                      Text(
+                        "Cybersecurity",   
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Roxborough-CF',
+                          fontSize: 50,
+                          color: Colors.white, //TODO pink color
+                          height: 1.2,
+                        ),
+                      ),
+                      Text(
+                        "TRAINING APP",    //Cybersecurity\n
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white, //TODO pink color
+                          height: 1.2,
+                        ),
+                      ),
+                      const SizedBox(height: 60),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFF4A5AE),
+                          padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        onPressed: () {
+                          // TODO go to next screen
+                        },
+                        child: const Text(
+                         'Enter',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                onPressed: () {
-                  // TODO go to next screen
-                },
-                child: Text(
-                  'Enter',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-            ],
-          ),
-        ),
+              )
+            )
+          ],
+        )
       ),
     );
   }
