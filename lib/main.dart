5f716b261eb2377d35734007a48eeada7f989d3c
Alexandra <64429585+mwalexandra@users.noreply.github.com>
2025-08-13 10:27:58 +0200

    web, tablet, mob-welcome screens added

--- lib/main.dart
+++ lib/main.dart
@@ -23,83 +23,21 @@ class CybersecurityApp extends StatelessWidget {
           brightness: Brightness.dark,
         )
       ),
-      home: WelcomeScreen(),
-    );
-  }
-}
-
-class WelcomeScreen extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal:24.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/images/logo.svg',
-                        width: 200,
-                        height: 200,
-                      ),
-                      const SizedBox(height: 60),
-                      Text(
-                        "Cybersecurity",   
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: 'Roxborough-CF',
-                          fontSize: 50,
-                          color: AppColors.textPrimary, 
-                          height: 1.2,
-                        ),
-                      ),
-                      Text(
-                        "TRAINING APP",    
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w400,
-                          color: AppColors.textPrimary, 
-                          height: 1.2,
-                        ),
-                      ),
-                      const SizedBox(height: 60),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.lightpink,
-                          padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 16),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context, 
-                            MaterialPageRoute(builder: (context) => const DescriptionScreen()),
-                          );
-                        },
-                        child: const Text(
-                         'Enter',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: AppColors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            )
-          ],
-        )
+      home: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            // Mobile layout
+            return MobWelcomeScreen();
+          } if (constraints.maxWidth < 1200) {
+            // Tablet layout
+            return TabletWelcomeScreen();
+          } else {
+            // Desktop layout
+            return WebWelcomeScreen();
+          }
+        },
       ),
     );
   }
 }
+
